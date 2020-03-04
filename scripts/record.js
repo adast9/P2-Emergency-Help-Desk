ba6ae6169d0397295d69276bbc4548cbadcf8419
adamstuck <56338480+adamstuck@users.noreply.github.com>
2020-03-04 14:32:23 +0100

    Updated audio record

--- scripts/record.js
+++ scripts/record.js
@@ -1,10 +1,12 @@
 let shouldStop = false;
 let stopped = false;
 const downloadLink = document.getElementById('download');
-const stopButton = document.getElementById('stop');
+const startButton = document.getElementById('start-audio-rec')
+const stopButton = document.getElementById('stop-audio-rec');
 
-stopButton.addEventListener('click', function() {
-    shouldStop = true;
+startButton.addEventListener('click', function() {
+    navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+        .then(handleSuccess);
 });
 
 const handleSuccess = function(stream) {
@@ -13,23 +15,17 @@ const handleSuccess = function(stream) {
     const mediaRecorder = new MediaRecorder(stream, options);
 
     mediaRecorder.addEventListener('dataavailable', function(e) {
-        if (e.data.size > 0) {
-            recordedChunks.push(e.data);
-        }
+        if (e.data.size > 0) recordedChunks.push(e.data);
+    });
 
-        if(shouldStop === true && stopped === false) {
-            mediaRecorder.stop();
-            stopped = true;
-        }
+    stopButton.addEventListener('click', function() {
+        mediaRecorder.stop();
     });
 
     mediaRecorder.addEventListener('stop', function() {
         downloadLink.href = URL.createObjectURL(new Blob(recordedChunks));
-        downloadLink.download = 'acetest.wav';
+        downloadLink.download = 'test.wav';
     });
 
     mediaRecorder.start();
 };
-
-navigator.mediaDevices.getUserMedia({ audio: true, video: false })
-  .then(handleSuccess);
