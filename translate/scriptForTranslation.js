a4aeee77a64f768428b29d21e19ab584f777be04
Bob <54476467+BobbyMcdonald@users.noreply.github.com>
2020-03-08 16:32:02 +0100

    Ændret google translate knappen

--- translate/scriptForTranslation.js
+++ translate/scriptForTranslation.js
@@ -1,6 +1,7 @@
 function googleTranslateElementInit() {
   new google.translate.TranslateElement(
-    {pageLanguage: "en"},                /*Her indsættes det sprog man ønsker at oversætte fra */
+    {pageLanguage: "en", layout:  /*Her indsættes det sprog man ønsker at oversætte fra */
+    google.translate.TranslateElement.InlineLayout.SIMPLE},
     "google_translate_element"
   );
 }
