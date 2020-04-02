4b83e8b007cf556e816754aeb271af7346f14b81
Kage4life <thors19@student.aau.dk>
2020-04-02 14:38:36 +0200

    more db fix

--- CMS-1/config/configuration.js
+++ CMS-1/config/configuration.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoDbUrl : 'mongodb://localhost:27017/cms',
+    mongoDbUrl : 'mongodb://localhost:27017/CMSTest',
     PORT: process.env.PORT || 3000,
     globalVariables: (req, res, next) => {
       res.locals.success_message = req.flash("success_message");
