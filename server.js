3a5adc0c95b07b8f2923ba3b0eb99f6e39149b79
adamstuck <adam.moloney.stuck@gmail.com>
2020-04-02 10:50:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,19 @@ console.log("Listening on port 25654...");
 // TODO: get markers to show on map with id / label
 
 s.on('connection', function(ws) {
+	ws.on('close', function() {
+		// When a client disconnects, check if they are in the EMD array. 
+		// If they are, remove them from the array.
+		let i = emds.indexOf(ws);
+		if (i !== -1) emds.splice(i, 1);
+	});
+
 	ws.on('message', function(data) {
 		// If a client sends the message "EMD", add them to the EMD array
 		// and send them all the current cases.
 		if(data == "EMD") {
 			emds.push(ws);
-			cases.forEach(function (entry) {
+			cases.forEach(function(entry) {
 				ws.send(JSON.stringify(entry));
 			});
 			return;
@@ -31,15 +38,10 @@ s.on('connection', function(ws) {
 		console.log("New case received! id: " + caseObject.id);
 		cases.push(caseObject);
 
-		emds.forEach(function (emd) {
+		emds.forEach(function(emd) {
 			emd.send(JSON.stringify(caseObject));
 		});
 	});
 
-	ws.on('close', function() {
-		// When a client disconnects, check if they are in the EMD array. 
-		// If they are, remove them from the array.
-		let i = emds.indexOf(ws);
-		if (i !== -1) emds.splice(i, 1);
-	});
+	
 });
