d2ef2c2c8263ad13f916431533b35d0e6f0a50ba
heylittleairplane <hejlesen@live.dk>
2020-04-15 13:24:09 +0200

    Creating folders

---
+++ scripts/public/websocket.js
@@ -0,0 +1,20 @@
+let ws = new WebSocket("ws://localhost:3001");
+submitButton.addEventListener('click', function(){ SubmitButton(); });
+
+ws.onopen = function() {
+    console.log("Connected to the server.");
+}
+
+function SubmitButton() {
+    if(markerPosition) {
+        SendToServer({
+            type: "CreateCase",
+            desc: document.getElementById('desc').value,
+            pos: markerPosition
+        });
+    }
+}
+
+function SendToServer(data) {
+    ws.send(JSON.stringify(data));
+}
