41f974913d5f0bf7acd09e714947b3e06467726b
heylittleairplane <58219966+heylittleairplane@users.noreply.github.com>
2020-05-21 13:42:35 +0200

    Update translation.js

--- references/scripts/translation.js
+++ references/scripts/translation.js
@@ -1,15 +1,9 @@
 // File information
 
 function googleTranslateElementInit() {
-	new google.translate.TranslateElement(
-		{pageLanguage: "en", layout:  /*Her indsættes det sprog man ønsker at oversætte fra */
-		google.translate.TranslateElement.InlineLayout.SIMPLE},
-		"google_translate_element"
-		);
-}
-
-function pushTextArea() {
-	let destination = document.getElementById("waitingForText");
-	let source = document.getElementById("textSource")
-	destination.innerHTML = source.value;
+	new google.translate.TranslateElement({
+		pageLanguage: "en", 
+		layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+	}, "google_translate_element"
+	);
 }
