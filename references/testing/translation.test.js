e86d3185ecd758f5ad0fbc37d4e8712e9ae7cdae
Bob <54476467+BobbyMcdonald@users.noreply.github.com>
2020-05-22 22:45:13 +0200

    test funktion for translate elementet

---
+++ references/testing/translation.test.js
@@ -0,0 +1,26 @@
+const puppeteer = require("puppeteer");
+
+test("should translate text from English to Danish", async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 200,
+        args: ["--window-size=1920,1080"]
+    })
+    const page = await browser.newPage();
+    await page.goto(
+        "file:///C:/Users/fred7/Documents/GitHub/P2-Emergency-Help-Desk/references/testing/mockBrowser.html"
+    );
+
+    await page.mouse.click(10, 20);
+
+    await page.mouse.click(250, 270);
+
+    const text = await page.$eval('#someText', (elem) => {
+    return elem.textContent;
+    });
+
+    expect(text).toBe("Tekst der skal oversættes.")
+
+    await browser.close();
+
+}, 10000);
